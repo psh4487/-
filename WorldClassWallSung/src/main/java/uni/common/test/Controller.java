f004b4e88ccd64ec1529d95475a9e928c573975a
una <kscxojs@gmail.com>
2020-08-10 15:18:04 +0900

    test

---
+++ WorldClassWallSung/src/main/java/uni/common/test/Controller.java
@@ -0,0 +1,9 @@
+package uni.common.test;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Controller {
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+}
